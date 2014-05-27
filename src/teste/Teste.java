8e24394b3c3d5245fff04909341bd981ea586495
vitor_gislon <vitor_gislon@10.1.48.249>
2014-05-27 14:35:15 -0300

    As belas flores do campo são azuis e inicializam o software!

---
+++ src/teste/Teste.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author vitor_gislon
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        JOptionPane.showMessageDialog(null, "Testando");
+    }
+}
